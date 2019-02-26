080a5ded00786a06e83f8c525b94280147bf5b01
Martin Petr <git@bodkan.net>
2019-02-27 00:47:57 +0100

    Add grid sims of init admixture

--- selection_simulations.sh
+++ selection_simulations.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+for init in `seq 0.01 0.01 0.25`; do
 for type in A X Y; do
-for rep in `seq 1 100`; do
-    N="archaic_chr${type}_sel_rep${rep}"
-    qsub -V -b yes -cwd -j y -l virtual_free=2G,h_vmem=2G,class=cow -o tmp/sge/${N}.out -N $N \
+for rep in `seq 1 25`; do
+    N="archaic_sel_chr${type}_rate${init}_rep${rep}"
+    qsub -V -b yes -cwd -j y -l virtual_free=5G,h_vmem=5G -o tmp/sge/${N}.out -N $N \
         slim \
             -d "chrom_type='${type}'" \
             -d mut_rate=1e-8 \
@@ -11,10 +12,11 @@ for rep in `seq 1 100`; do
             -d gene_total=3000000 \
             -d gene_count=60 \
             -d admix_time=55000 \
-            -d archaic_rate=0.1 \
+            -d archaic_rate=${init} \
             -d modern_rate=0 \
             -d "output='data/sim/${N}.txt'" \
             src/admixture.slim
+done
 done
 done
 
