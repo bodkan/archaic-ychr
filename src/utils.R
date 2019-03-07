d472b12b2d37ae005eb7c20278a645f1233d8170
Martin Petr <git@bodkan.net>
2019-03-07 16:52:22 +0100

    Fix VCF parsing code

--- src/utils.R
+++ src/utils.R
@@ -2,11 +2,13 @@ library(VariantAnnotation)
 library(tidyverse)
 
 # path = "data/vcf/merged_full.vcf.gz"
+# path = "data/vcf/test_gt.vcf.gz"
 # mindp = 4
 read_gt <- function(path, mindp = 0) {
   vcf <- readVcf(path)
 
-  gr_vcf <- granges(vcf) %>% .[elementNROWS(.$ALT) == 1]
+  bialellic_pos <- elementNROWS(granges(vcf)$ALT) == 1
+  gr_vcf <- granges(vcf)[bialellic_pos]
 
   info_df <- tibble(
       chrom = as.character(seqnames(gr_vcf)),
@@ -15,10 +17,10 @@ read_gt <- function(path, mindp = 0) {
       ALT = as.character(unlist(gr_vcf$ALT))
   )
 
-  dp_mask <- geno(vcf)$DP %>% apply(2, function(i) ifelse(i >= mindp, i, NA))
+  dp_mask <- geno(vcf)$DP %>% .[bialellic_pos, ] %>% apply(2, function(i) ifelse(i >= mindp, i, NA))
   if ("chimp" %in% samples(header(vcf))) dp_mask[, "chimp"] <- 1
 
-  gt_mat <- geno(vcf)$GT %>% replace(. == ".", NA) %>% replace(is.na(dp_mask), NA)
+  gt_mat <- geno(vcf)$GT %>% .[bialellic_pos, ] %>% replace(. == ".", NA) %>% replace(is.na(dp_mask), NA)
   mode(gt_mat) <- "numeric"
   gt_df <- gt_mat %>% as_tibble %>% mutate(reference = 0)
 
