2a69330dfe8340c78aec40d198c257fd2036067e
Martin Petr <git@bodkan.net>
2019-03-07 14:47:46 +0100

    Fix filtering of Chimp DP field and keep only 1 ALT sites

--- src/utils.R
+++ src/utils.R
@@ -1,18 +1,18 @@
-read_gt <- function(path) {
+read_gt <- function(path, mindp = 0) {
   vcf <- readVcf(path)
 
-  gr_vcf <- granges(vcf)
+  gr_vcf <- granges(vcf) %>% .[elementNROWS(.$ALT) == 1]
 
   info_df <- tibble(
-      chrom = levels(seqnames(gr_vcf)),
+      chrom = as.character(seqnames(gr_vcf)),
       pos = start(gr_vcf),
       REF = as.character(gr_vcf$REF),
       ALT = as.character(unlist(gr_vcf$ALT))
   )
 
-  gt_mat <- geno(vcf)$GT %>% replace(. == ".", NA)
-  mode(gt_mat) <- "numeric"
-  gt_df <- as_tibble(gt_mat) %>% mutate(reference = 0)
+  dp_mask <- geno(vcf)$DP %>% apply(2, function(i) ifelse(i >= mindp, i, NA))
+  if ("chimp" %in% samples(header(vcf))) dp_mask[, "chimp"] <- 1
+  gt_df <- geno(vcf)$GT %>% replace(is.na(dp_mask), NA) %>% as_tibble %>% mutate(reference = 0)
 
   bind_cols(info_df, gt_df)
 }
