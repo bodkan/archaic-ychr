f0e1dfbce276d08036b7d4535e09de755555f6e6
Martin Petr <git@bodkan.net>
2019-03-20 10:22:39 +0100

    Add TV-only filtering to read_gt

--- src/utils.R
+++ src/utils.R
@@ -1,18 +1,18 @@
 library(VariantAnnotation)
 library(tidyverse)
 
-# path = "data/vcf/merged_full.vcf.gz"
+# path = "data/vcf/merged_exome.vcf.gz"
 # path = "data/vcf/test_gt.vcf.gz"
 # mindp = 4
-read_gt <- function(path, mindp = 0, var_only = FALSE) {
+read_gt <- function(path, mindp = 0, var_only = FALSE, tv_only = FALSE) {
   vcf <- readVcf(path)
 
-  biallelic_pos <- elementNROWS(granges(vcf)$ALT) == 1
+  keep_pos <- elementNROWS(granges(vcf)$ALT) == 1
 
   if (var_only)
-    biallelic_pos <- biallelic_pos & unlist(granges(vcf)$ALT) != ""
+    keep_pos <- keep_pos & unlist(granges(vcf)$ALT) != ""
 
-  gr_vcf <- granges(vcf)[biallelic_pos]
+  gr_vcf <- granges(vcf)[keep_pos]
 
   info_df <- tibble(
       chrom = as.character(seqnames(gr_vcf)),
@@ -21,15 +21,21 @@ read_gt <- function(path, mindp = 0, var_only = FALSE) {
       ALT = as.character(unlist(gr_vcf$ALT))
   )
 
-  dp_mask <- geno(vcf)$DP[biallelic_pos, , drop = FALSE] %>% apply(2, function(i) ifelse(i >= mindp, i, NA))
+  dp_mask <- geno(vcf)$DP[keep_pos, , drop = FALSE] %>% apply(2, function(i) ifelse(i >= mindp, i, NA))
   if ("chimp" %in% samples(header(vcf))) dp_mask[, "chimp"] <- 1
 
-  gt_mat <- geno(vcf)$GT[biallelic_pos, , drop = FALSE] %>% replace(. == ".", NA) %>% replace(is.na(dp_mask), NA)
+  gt_mat <- geno(vcf)$GT[keep_pos, , drop = FALSE] %>% replace(. == ".", NA) %>% replace(is.na(dp_mask), NA)
   mode(gt_mat) <- "numeric"
   gt_df <- gt_mat %>% as_tibble %>% mutate(reference = 0)
 
   # sanitize the sample names
   colnames(gt_df) <- str_replace_all(colnames(gt_df), "-", "_")
 
-  bind_cols(info_df, gt_df)
+  res_df <- bind_cols(info_df, gt_df)
+
+  if (tv_only) {
+    res_df <- filter(res_df,  !((REF == "C" & ALT == "T") | (REF == "G" & ALT == "A")))
+  }
+
+  res_df
 }
