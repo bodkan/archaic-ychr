0667055736a508e3e110a48a9a19384daadbe99f
Martin Petr <git@bodkan.net>
2019-03-21 22:49:04 +0100

    Extract functions from nb to utils.R

--- src/utils.R
+++ src/utils.R
@@ -39,3 +39,60 @@ read_gt <- function(path, mindp = 0, var_only = FALSE, tv_only = FALSE) {
 
   res_df
 }
+
+
+
+#
+# Generate table of sample names and their population assignments and ages
+#
+sample_info <- function(gt) {
+    emh <- tibble(
+        name = c("ustishim", "bichon", "kk1", "loschbour", "mota"),
+        age = c(45000, 13665, 9720, 8050, 4500),
+        pop = "EMH"
+    )
+
+    modern <- tibble(
+        name = c(str_subset(colnames(gt), "^S_"), "reference", "a00", "a00_1", "a00_2"),
+        pop = case_when(
+            name == "reference" ~ "reference",
+            name %in% c("S_Burmese_1", "S_Thai_1", "S_Han_2", "S_Dai_2", "S_Punjabi_1", "S_Papuan_2", "S_Karitiana_1") ~ "EastEur",
+            name %in% c("S_BedouinB_1", "S_Turkish_1", "S_French_1", "S_Finnish_2", "S_Sardinian_1", "S_Saami_2") ~ "WestEur",
+            name %in% c("ustishim", "bichon", "kk1", "loschbour", "mota") ~ "EMH",
+            TRUE ~ "Africa"
+        ),
+        age = 0
+    )
+
+    bind_rows(emh, modern)
+}
+
+
+
+#
+# Assign mutations to all possible branches of a tree with 4 leaves.
+#
+site_patterns <- function(df, w, x, y, z) {
+    tibble(
+        a = as.integer(df[[w]] == df[[x]] & df[[y]] == df[[z]] & df[[w]] != df[[y]]),
+        b = as.integer(df[[w]] == df[[z]] & df[[x]] == df[[y]] & df[[w]] != df[[x]]),
+        c = as.integer(df[[w]] == df[[y]] & df[[x]] == df[[z]] & df[[w]] != df[[z]]),
+        d = as.integer(df[[w]] == df[[x]] & df[[w]] == df[[z]] & df[[w]] != df[[y]]),
+        e = as.integer(df[[w]] == df[[x]] & df[[w]] == df[[y]] & df[[w]] != df[[z]]),
+        f = as.integer(df[[w]] == df[[y]] & df[[w]] == df[[z]] & df[[w]] != df[[x]])
+    )
+}
+
+
+
+#
+# Count the occurences of different site patterns.
+#
+sum_patterns <- function(gt, w, x, y, z) {
+    df <- gt[, c(w, x, y, z)] %>% .[complete.cases(.), ]
+    site_patterns(df, w, x, y, z) %>% summarise_all(sum) %>% mutate(total = nrow(df))
+}
+
+
+
+
