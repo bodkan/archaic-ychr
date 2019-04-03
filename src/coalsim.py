f9daa5d890b4711e8519b10f5a5634cb9502c202
Martin Petr <git@bodkan.net>
2019-04-03 18:08:32 +0200

    Simulate just one UI

--- src/coalsim.py
+++ src/coalsim.py
@@ -7,7 +7,7 @@ import msprime as msp
 
 
 def years_to_gen(y):
-    return int(y  /25)
+    return int(y  / 25)
 
 
 def define_samples(pop_params):
@@ -35,17 +35,17 @@ parser.add_argument("--Ne-afr", type=int, default=100)
 parser.add_argument("--Ne-arch", type=int, default=10)
 
 parser.add_argument("--seq-len", help="Sequence length", type=int, default=7_000_000)
-parser.add_argument("--mut-rate", help="Mutation rate", type=float, default=7.6e-10)
+parser.add_argument("--mut-rate", help="Mutation rate", type=float, default=7.6e-9)
 
-parser.add_argument("--emh-ages", nargs="+", type=int, help="Ages of EMH samples [years BP]")
+parser.add_argument("--ui-age", nargs="+", type=int, help="Age of the Ust-Ishim individual [years BP]")
 parser.add_argument("--arch-ages", nargs="+", type=int, help="Ages of archaic samples [years BP]", required=True)
 parser.add_argument("--neur", type=int, help="Number of European chromosomes to simulate", required=True)
 parser.add_argument("--nasn", type=int, default=0, help="Number of Asian chromosomes to simulate")
 parser.add_argument("--nafr", type=int, default=1, help="Number of African chromosomes to simulate")
 
 parser.add_argument("--split-chimp", type=int, default=6_000_000)
-parser.add_argument("--split-arch", type=int, default=600_000)
-parser.add_argument("--split-eur", type=int, default=70_000)
+parser.add_argument("--split-arch", type=int, default=650_000)
+parser.add_argument("--split-eur", type=int, default=250_000)
 parser.add_argument("--split-asn", type=int, default=45_000)
 
 parser.add_argument("--scale", type=float, default=1.0)
@@ -55,13 +55,13 @@ parser.add_argument("--output", metavar="FILE", help="Output filename")
 parser.add_argument("--debug", action="store_true", help="Print debugging info")
 
 # args = parser.parse_args()
-args = parser.parse_args("--debug --arch-ages 130000 50000 --emh-ages 45000 45000 45000 --neur 5 --nafr 5 --output out.tsv".split())
+args = parser.parse_args("--debug --arch-ages 130000 50000 --ui-age 45000 --neur 5 --nafr 5 --output out.tsv".split())
 
 pop_params = {
     "chimp": {"id": 0, "Ne": 10000, "t_sample": 1 * [0], "t_split" : args.split_chimp},
     "arch": {"id": 1, "Ne": args.Ne_arch,  "t_sample": args.arch_ages, "t_split": args.split_arch},
     "afr": {"id": 2, "Ne": args.Ne_afr, "t_sample": args.nafr * [0]},
-    "eur": {"id": 3, "Ne": args.Ne_nonafr, "t_sample": args.emh_ages + args.neur * [0], "t_split": args.split_eur},
+    "eur": {"id": 3, "Ne": args.Ne_nonafr, "t_sample": args.ui_age + args.neur * [0], "t_split": args.split_eur},
     "asn": {"id": 4, "Ne": args.Ne_nonafr, "t_sample": args.nasn * [0], "t_split": args.split_asn}
 }
 
