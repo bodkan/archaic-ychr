8afc92900fda2ec0d2d14f5b36055041fe892530
Martin Petr <git@bodkan.net>
2019-04-04 18:35:47 +0200

    Add coalsim pipeline

---
+++ coalsims.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p data/coalsims
+
+for split in `seq 300000 10000 800000`; do
+for rep in {1..10}; do
+  ./src/coalsim.py --split-arch ${split} --split-afr 250000 \
+    --arch-ages 130000 --ui-age 45000 --neur 5 --nafr 5 --nasn 5 \
+    --format snp --output data/coalsims/archsplit${split}_rep${rep}.tsv
+done
+done
