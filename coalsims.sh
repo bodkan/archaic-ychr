e3cc59fed036b7f299befb50cd266c441cef9246
Martin Petr <git@bodkan.net>
2019-04-05 17:49:34 +0200

    qsub -b no

--- coalsims.sh
+++ coalsims.sh
@@ -5,7 +5,7 @@ mkdir -p data/coalsims
 for split in `seq 300000 25000 800000`; do
 for rep in {1..10}; do
   N=archsplit${split}_afrsplit250000_rep${rep}
-  qsub -V -b yes -cwd -j y -l virtual_free=1G,h_vmem=1G -o tmp/sge/${N}.out -N $N \
+  qsub -V -cwd -j y -l virtual_free=1G,h_vmem=1G -o tmp/sge/${N}.out -N $N \
     ./src/coalsim.py --split-arch ${split} --split-afr 250000 \
       --arch-ages 130000 --ui-age 45000 --neur 5 --nafr 5 --nasn 5 \
       --format snp --output data/coalsims/${N}.tsv
