3b92654e10e21204dddf55cadff007c9f1b52036
Martin Petr <git@bodkan.net>
2019-04-05 17:33:36 +0200

    Update coalsim pipeline

--- coalsims.sh
+++ coalsims.sh
@@ -6,6 +6,6 @@ for split in `seq 300000 25000 800000`; do
 for rep in {1..10}; do
   ./src/coalsim.py --split-arch ${split} --split-afr 250000 \
     --arch-ages 130000 --ui-age 45000 --neur 5 --nafr 5 --nasn 5 \
-    --format snp --output data/coalsims/archsplit${split}_rep${rep}.tsv
+    --format snp --output data/coalsims/archsplit${split}_afrsplit250000_rep${rep}.tsv
 done
 done
