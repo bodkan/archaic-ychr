270c1b299178b52631ed0812243baea0964749bf
Martin Petr <git@bodkan.net>
2019-04-06 14:28:16 +0200

    Rename functions

--- R/tmrca.R
+++ R/tmrca.R
@@ -21,7 +21,7 @@ sum_patterns <- function(gt, w, x, y, z) {
 
 #' Calculate T_MRCA of all pairs of Africans and non-Africans, using a set
 #' of EMHs of known age for branch shortening-based mutation rate estimation.
-calculate_tAfrRef <- function(gt, samples) {
+calculate_tafr <- function(gt, samples) {
     refs <- filter(samples, pop != "Africa", pop != "EMH")$name
     afrs <- filter(samples, pop == "Africa")$name
     emhs <- filter(samples, pop == "EMH")$name
@@ -40,20 +40,21 @@ calculate_tAfrRef <- function(gt, samples) {
         mut_rate = muts_per_year / total,
         tmrca_ad = (a + d) / (mut_rate * total),
         tmrca_f = f / (mut_rate * total),
-        tmrca_avg = (tmrca_ad + tmrca_f) / 2
-    )
+        tmrca_afr = (tmrca_ad + tmrca_f) / 2
+    ) %>%
+    select(afr, ref, tmrca_afr, tmrca_ad, tmrca_f, mut_rate, everything())
 }
 
 
 #' Calculate T_MRCA of each of the specified archaic individuals
 #' with modern human Y chromosomes.
-calculate_tArchRef <- function(gt, samples, tAfrRef) {
+calculate_tarch <- function(gt, samples, tafr) {
     refs <- filter(samples, pop != "Africa", pop != "EMH")$name
     afrs <- filter(samples, pop == "Africa")$name
     archaics <- colnames(select(gt, -c(chrom, pos, REF, ALT, chimp), -one_of(samples$name)))
 
-    tAfrRef_ui <- filter(tAfrRef, emh == "ustishim") %>%
-        select(afr, ref, mut_rate, tmrca_ad, tmrca_f, tmrca_avg)
+    tafr_ui <- filter(tafr, emh == "ustishim") %>%
+        select(afr, ref, mut_rate, tmrca_ad, tmrca_f, tmrca_afr)
 
     map_dfr(refs, function(ref) {
         map_dfr(afrs, function(afr) {
@@ -62,9 +63,9 @@ calculate_tArchRef <- function(gt, samples, tAfrRef) {
                     mutate(arch = arch, afr = afr, ref = ref)
             })
         })
-    }) %>% inner_join(tAfrRef_ui, by = c("afr", "ref")) %>%
+    }) %>% inner_join(tafr_ui, by = c("afr", "ref")) %>%
     mutate(p = a / (a + d + e),
            alpha = (1 + p) / (1 - p),
-           tmrca_arch = tmrca_avg * alpha) %>%
-    select(arch, afr, ref, tmrca_arch, alpha, mut_rate, tmrca_ad, tmrca_f, tmrca_avg, everything())
+           tmrca_arch = tmrca_afr * alpha) %>%
+    select(arch, afr, ref, tmrca_arch, alpha, mut_rate, tmrca_afr, tmrca_ad, tmrca_f, everything())
 }
