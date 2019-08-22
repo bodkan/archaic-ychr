06937df38323c69a2c34bba53f5b190a316a4f1a
Martin Petr <git@bodkan.net>
2019-08-22 15:28:46 +0200

    Run new selection simulations

--- selection_simulations.sh
+++ selection_simulations.sh
@@ -1,55 +1,25 @@
 #!/usr/bin/env bash
 
-for init in `seq 0.01 0.01 0.25`; do
-for type in Y; do
+for chrom in Y; do
+for gene_total in `seq 100000 100000 2000000`; do
+for admix_rate in `seq 0.01 0.01 0.1`; do
+for admix_time in `seq 200000 50000 450000`; do
 for rep in `seq 1 25`; do
-    N="archaic2modern_sel_chr${type}_rate${init}_rep${rep}"
+    N="modern2neand_sel_chr${chrom}_seq${gene_total}_rate${admix_rate}_time${admix_time}_rep${rep}"
     qsub -V -b yes -cwd -j y -l virtual_free=5G,h_vmem=5G -o tmp/sge/${N}.out -N $N \
         slim \
-            -d "chrom_type='${type}'" \
-            -d mut_rate=1e-8 \
+            -d "chrom='${chrom}'" \
+            -d mut_rate=1.85e-08 \
             -d chrom_length=60000000 \
-            -d gene_total=3000000 \
-            -d gene_count=60 \
-            -d admix_time=55000 \
-            -d archaic_rate=${init} \
-            -d modern_rate=0 \
-            -d "output='data/sim/${N}.txt'" \
-            src/admixture.slim
+            -d gene_count=100 \
+            -d gene_total=${gene_total} \
+            -d admix_time=${admix_time} \
+            -d modern_neand=${admix_rate} \
+            -d neand_modern=0 \
+            -d "output='data/newsim/${N}.txt'" \
+            src/selection.slim
 done
 done
 done
-
-
-for init in `seq 0.01 0.01 0.25`; do
-for type in Y; do
-for rep in `seq 1 25`; do
-    N="modern2archaic_sel_chr${type}_rate${init}_rep${rep}"
-    qsub -V -b yes -cwd -j y -l virtual_free=5G,h_vmem=5G -o tmp/sge/${N}.out -N $N \
-        slim \
-            -d "chrom_type='${type}'" \
-            -d mut_rate=1e-8 \
-            -d chrom_length=60000000 \
-            -d gene_total=3000000 \
-            -d gene_count=60 \
-            -d admix_time=55000 \
-            -d archaic_rate=0 \
-            -d modern_rate=${init} \
-            -d "output='data/sim/${N}.txt'" \
-            src/admixture.slim
 done
 done
-done
-
-
-# slim -d 'chrom_type="A"' \
-#      -d mut_rate=1e-8 \
-#      -d chrom_length=60000000 \
-#      -d gene_total=3000000 \
-#      -d gene_count=60 \
-#      -d admix_time=55000 \
-#      -d archaic_rate=0.1 \
-#      -d modern_rate=0 \
-#      -d 'output="asd.txt"' \
-#      src/admixture.slim
-
