ade17f1320b8219264a85375b67d20ce3742369e
Martin Petr <git@bodkan.net>
2019-08-31 23:53:53 +0200

    Add option for scaling the DFE in SLiM simulations

--- selection_simulations.sh
+++ selection_simulations.sh
@@ -26,6 +26,39 @@ done
 done
 
 
+
+
+# simulations exploring the DFE vs amount of sequence
+
+mkdir -p data/sim/DFE
+
+for scale in `seq 1.0 1.0 10.0`; do
+for gene_total in `seq 100000 100000 2000000`; do
+for admix_rate in 0.03; do
+for admix_time in `seq 200000 25000 450000`; do
+for rep in `seq 1 5`; do
+    N="dfe_${scale}_seq${gene_total}_time${admix_time}_rate${admix_rate}_rep${rep}"
+    qsub -V -b yes -cwd -j y -l virtual_free=1G,h_vmem=1G -o tmp/sge/${N}.out -N $N \
+        slim \
+            -d mut_rate=1.85e-08 \
+            -d gene_total=${gene_total} \
+            -d "direction='modern2neand'" \
+            -d admix_time=${admix_time} \
+            -d admix_rate=${admix_rate} \
+            -d dump_at=$((${admix_time} + 25)) \
+            -d sample_for=100000 \
+            -d "output='data/sim/DFE/${N}'" \
+            src/selection.slim
+done
+done
+done
+done
+done
+
+
+
+# simulations exploring the admixture from Neanderthals into AMH
+
 direction="neand2modern"
 mkdir -p data/sim/${direction}
 
