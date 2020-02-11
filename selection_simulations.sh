4d78a9a7e945c1634060711959788314b3e6d460
Martin Petr <git@bodkan.net>
2020-02-11 17:02:12 +0100

    Run simulations starting from 175 kya only
    Running them from 150 kya clashes with downstream time points. The
    simulations are tracked for 100 ky, but modern humans are already
    removed before that. This leads to crashes.

--- selection_simulations.sh
+++ selection_simulations.sh
@@ -6,7 +6,7 @@ mkdir -p data/sim/${direction}
 
 for gene_total in `seq 100000 100000 2000000`; do
 for admix_rate in 0.05; do
-for admix_time in `seq 200000 25000 450000`; do
+for admix_time in `seq 175000 25000 450000`; do
 for rep in `seq 1 100`; do
     N="seq${gene_total}_time${admix_time}_rate${admix_rate}_rep${rep}"
     qsub -V -b yes -cwd -j y -l virtual_free=5G,h_vmem=5G -o tmp/sge/${N}.out -N $N \
